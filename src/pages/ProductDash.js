eb9d9172d39ff683315d5cc0c9c765a3cba16c1a
Antoine Debes <Antoinedebes@gmail.com>
2021-08-18 10:26:55 +0300

    Pagination

--- src/pages/ProductDash.js
+++ src/pages/ProductDash.js
@@ -2,29 +2,59 @@ import React, { useState, useEffect } from "react";
 import ProductHeader from "../components/ProductHeader";
 import ProductCard from "../components/ProductCard";
 import { productApi } from "../apis/Api";
+import ReactPaginate from "react-paginate";
 
 export default function ProductDash() {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0);
+  // const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     productApi()
       .then((res) => {
-        console.log({ res });
         setProducts(res.data.products);
+        getData();
       })
       .catch((error) => {
         console.log("error", error);
       });
   }, []);
+
+  const getData = () => {
+    console.log({ products });
+    const data = products;
+    const slice = data.slice(offset, offset + perPage);
+    const postData = slice.map((product) => (
+      <ProductCard key={product} productInfo={product} />
+    ));
+    setData(postData);
+    setPageCount(Math.ceil(data.length / perPage));
+  };
+  const changePage = ({ selected }) => {
+    setOffset(selected);
+  };
+
+  useEffect(() => {
+    getData();
+  }, [products, offset, pageCount]);
   return (
     <>
       <div class="content__container">
         <ProductHeader />
-        {products &&
-          products.map((product) => {
-            console.log(product);
-            return <ProductCard productInfo={product} />;
-          })}
+        {data && data}
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={pageCount}
+          onPageChange={changePage}
+          containerClassName={"paginationBttns"}
+          previousLinkClassName={"previousBttn"}
+          nextLinkClassName={"nextBttn"}
+          activeClassName={"paginationActive"}
+        />
       </div>
     </>
   );
