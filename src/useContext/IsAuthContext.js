94ae114302878874b4dc19dfaa69f5717d873ef5
Kevin <kevin.hanna@profitisle.com>
2021-08-30 21:53:55 +0300

    Test

--- src/useContext/IsAuthContext.js
+++ src/useContext/IsAuthContext.js
@@ -2,12 +2,14 @@ import React, { useState, useContext, createContext } from "react";
 
 const IsAuthContext = createContext();
 
+
+
 export function useAuth() {
   return useContext(IsAuthContext);
 }
 
 export function IsAuthProvider({ children }) {
-  const localStorageIsAuth = localStorage.getItem("isAuth") === "true";
+  const localStorageIsAuth = true
   const [isAuth, setIsAuth] = useState(localStorageIsAuth);
   return (
     <>
