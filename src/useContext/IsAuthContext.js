8fe701f51ebd6eebab2914e051c600abe85aec24
Antoinedebes <antoinedebes@gmail.com>
2021-09-05 12:08:28 +0300

    fix: isAuth.js localstorage setting the isAuth content

--- src/useContext/IsAuthContext.js
+++ src/useContext/IsAuthContext.js
@@ -8,7 +8,6 @@ export function useAuth() {
 
 export function IsAuthProvider({ children }) {
   const localStorageIsAuth = localStorage.getItem("isAuth") === "true";
-
   const [isAuth, setIsAuth] = useState(localStorageIsAuth);
 
   return (
